71aefa187c86ed2004207bb3776f4985af6421ee
bagusyanuar <bagus.yanuar613@gmail.com>
2026-07-08 22:04:24 +0700

    feat(core): implement central request validation
    - Setup go-playground validator with Indonesian translations
    
    - Integrate universal-translator for robust formatting
    
    - Configure air build tool for multi-file main package
    
    - Update gitignore to exclude tmp and api binaries

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.26.3
 require (
 	github.com/andybalholm/brotli v1.2.2 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.30.3 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/gofiber/fiber/v3 v3.4.0 // indirect
 	github.com/gofiber/schema v1.8.0 // indirect
@@ -18,6 +22,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.19.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.15 // indirect
 	github.com/mattn/go-isatty v0.0.22 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
---
+++ pkg/validator/validator.go
@@ -0,0 +1,60 @@
+package validator
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/locales/id"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	id_translations "github.com/go-playground/validator/v10/translations/id"
+)
+
+// Validate is a global singleton for the validator.
+var Validate *validator.Validate
+var trans ut.Translator
+
+func init() {
+	Validate = validator.New()
+
+	// Setup translator for Indonesian locale
+	idLocale := id.New()
+	uni := ut.New(idLocale, idLocale)
+	trans, _ = uni.GetTranslator("id")
+
+	// Register default Indonesian translations
+	_ = id_translations.RegisterDefaultTranslations(Validate, trans)
+
+	// Register tag name function to extract the 'json' tag name instead of the struct field name.
+	Validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		if name == "" {
+			return fld.Name
+		}
+		return name
+	})
+}
+
+// ValidateStruct validates a given struct and returns a map of human-readable field errors (Laravel format).
+// It returns nil if there are no errors.
+func ValidateStruct(data interface{}) map[string][]string {
+	err := Validate.Struct(data)
+	if err == nil {
+		return nil
+	}
+
+	errors := make(map[string][]string)
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		for _, e := range validationErrors {
+			field := e.Field()
+			errors[field] = append(errors[field], e.Translate(trans))
+		}
+	} else {
+		// If it's not a ValidationErrors type, return a generic error.
+		errors["general"] = []string{err.Error()}
+	}
+	return errors
+}
